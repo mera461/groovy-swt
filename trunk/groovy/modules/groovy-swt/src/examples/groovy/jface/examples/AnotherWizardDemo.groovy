5afd57a95b5f33ba126e181beb37b3d71653be95
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2008-05-20 18:50:21 +0000

    Updated all examples.

---
+++ trunk/groovy/modules/groovy-swt/src/examples/groovy/jface/examples/AnotherWizardDemo.groovy
@@ -0,0 +1,112 @@
+package groovy.jface.examples
+
+import groovy.jface.JFaceBuilder 
+import org.eclipse.swt.layout.GridData
+
+/**
+ * @author Alexander Becher
+ */
+class AnotherWizardDemo {
+
+	def jface = new JFaceBuilder()
+	def wizardPage2
+	
+  public static void main(String[] args) {
+    def demo = new AnotherWizardDemo()
+    demo.run()
+  }
+  
+	void run() {
+		def dates =[ "1", "2", "3", "4", "5", "6", "7", "8", "9",
+		     		"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+		     		"21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
+		         
+		def  months= ["January", "February", "March", "April", "May",
+		     		"June", "July", "August", "September", "October", "November", "December" ]
+		// lets create a window
+		def mainapp = jface.window() {
+			// and now the wizard dialog
+	  		def wizardDialog = wizardDialog(text:"Groovy Wizard Demo", 
+	  				performFinish:{ println "Finished was pressed"; return true}, 
+	  				performCancel:{ println "Cancel method"; return true} ) 
+	  		{
+				// lets add a picture to the wizard's title
+				// TODO: Make the image work.
+				image(src:"groovy.gif")
+				// first page for the wizard
+		  		def wizardPage1 = wizardPage( title:"Step 1", description:"This is the first page", 
+		  				nextPressed:{ println "Button Next on first Page" }, closure: 
+		  		{ 
+					
+	  				jface.composite(it) {
+						gridLayout( numColumns:4 )
+					     
+					    // Date of travel
+					  	def Calendar cal = Calendar.getInstance()
+					  	cal.setTime(new Date())
+				        def day = cal.get(Calendar.DAY_OF_MONTH)
+						label (  text:"Travel on:", layoutData:gridData(horizontalAlignment:GridData.BEGINNING))
+					    def travelDate = combo( style:"READ_ONLY", items:dates, visibleItemCount:10, layoutData:gridData(horizontalAlignment:GridData.FILL, widthHint:25))
+					    travelDate.text=day
+						def travelMonth = combo(  style:"READ_ONLY", items: months, layoutData:gridData(horizontalAlignment:GridData.FILL))
+						travelMonth.text = travelMonth.items[cal.get(Calendar.MONTH)]
+						// Calculate years 	
+						def startyear = cal.get(Calendar.YEAR);
+						def years = []
+						years.add(startyear)
+						(1..<3).each {
+							years.add(startyear + it)
+						}
+						
+						def travelYear = combo( style:"READ_ONLY", items: years, layoutData:gridData(horizontalAlignment:GridData.FILL))
+					   	travelYear.text = startyear
+						
+						// Date of return		
+						label (style:"NONE", text:"Return on:", layoutData:gridData(horizontalAlignment:GridData.FILL))
+
+						combo(style:"READ_ONLY", items: dates, layoutData:gridData(horizontalAlignment:GridData.FILL, widthHint:25))
+						combo( style:"READ_ONLY", items:months, layoutData:gridData(horizontalAlignment:GridData.FILL))
+						combo(style:"READ_ONLY", items:years, layoutData:gridData(horizontalAlignment:GridData.FILL))
+						
+						// Departure				
+						label (style:"NONE", text:"From:", layoutData:gridData(horizontalAlignment:GridData.FILL))			
+						text( style:"BORDER", layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3));
+
+						// Destination
+						label (style:"NONE", text:"To:", layoutData:gridData(horizontalAlignment:GridData.FILL))				
+						text( style:"BORDER", layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3))
+						
+						radioButton(  text:"Take a plane", layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3))
+						radioButton( text: "Take the car", layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3))
+						line (layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3))
+					}
+	  			})
+	  			// second page for the wizard
+	  			wizardPage2 = wizardPage( title:"Step 2", description:"This is the second page", 
+	  					backPressed:{println "back on 2nd page"},
+	  					closure: { 
+						jface.composite( it ) {
+							gridLayout( numColumns:"2" )
+							label( text:"Set PageComplete to true/false: " )
+							    def button2 = button(style:"PUSH", text:"Change it") {
+        							onEvent(type:"Selection", closure:{ 
+							        	wizardPage2.setPageComplete(!wizardPage2.isPageComplete() )
+							        })
+							    }
+						}
+				})
+				def wizardPage3 = wizardPage( title:"Step 3", description:"This is the third page", closure: { 
+						jface.composite( it ) {
+							gridLayout( numColumns:"2" )
+							label( text:"Label 3" )
+							button( text:"Do nothing" )
+						}
+				})
+					
+	  		}
+	  		wizardPage2.setPageComplete(false)
+	  		wizardDialog.open()
+		}		
+	}
+
+}
---
+++ trunk/groovy/modules/groovy-swt/src/examples/groovy/jface/examples/SimpleWizardDemo.groovy
@@ -0,0 +1,46 @@
+package groovy.jface.examples
+
+import groovy.jface.JFaceBuilder
+
+
+/**
+ * @author Alexander Becher
+ */
+class SimpleWizardDemo {
+
+	def jface = new JFaceBuilder()
+	
+	public static void main(String[] args) {
+		def demo = new SimpleWizardDemo()
+		demo.run()
+	}
+	  
+	void run() {
+		
+		def mainapp = jface.window() {
+			// and now the wizard dialog
+	  		def wizardDialog = wizardDialog(text:"Wizard Demo") 
+	  		{ image(src:"groovy.gif")
+				// first page for the wizard
+		  		def wizardPage1 = wizardPage( title:"Step 1", description:"This is the first page", closure: 
+		  		{ 
+	  				jface.composite(it) {
+	  					gridLayout( numColumns:"2" )
+					    label(text:"label on first page")
+					    button(text:"do nothing")
+					}
+	  			})
+	  			
+	  			// second page for the wizard
+	  			def wizardPage2 = wizardPage( title:"Step 2", description:"This is the second page", closure: { 
+					jface.composite( it ) {
+						gridLayout( numColumns:"2" )
+					    label(text:"label on second page")
+					    button(text:"do nothing")							}
+				})
+				
+	  		}
+			wizardDialog.open()
+		}		
+	}
+}
