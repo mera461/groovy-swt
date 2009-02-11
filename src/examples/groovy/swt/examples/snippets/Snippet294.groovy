c23ef948adefce7680ed78b560d15de06384646e
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-02-11 12:11:09 +0000

    Eclipse 3.4 snippets.

---
+++ src/examples/groovy/swt/examples/snippets/Snippet294.groovy
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package groovy.swt.examples.snippets
+
+import groovy.swt.SwtBuilder
+import org.eclipse.swt.graphics.Region
+
+/**
+ * For testing a 3.4 new feature: Buttons with regions.
+ * 
+ * @author Frank
+ *
+ */
+public class Snippet294{
+
+	def swt = new SwtBuilder()
+	def shell
+	
+    public static void main(String[] args) {
+		def demo = new Snippet294()
+		demo.run()
+	}
+	
+	static int[] circle(int r, int offsetX, int offsetY) {
+		int[] polygon = new int[8 * r + 4];
+		// x^2 + y^2 = r^2
+		for (int i = 0; i < 2 * r + 1; i++) {
+			int x = i - r;
+			int y = (int)Math.sqrt(r*r - x*x);
+			polygon[2*i] = offsetX + x;
+			polygon[2*i+1] = offsetY + y;
+			polygon[8*r - 2*i - 2] = offsetX + x;
+			polygon[8*r - 2*i - 1] = offsetY - y;
+		}
+		return polygon;
+	}
+	
+	void run(){
+		
+		// define a region that looks like a circle with two holes in ot
+		Region region = new Region();
+		region.add(circle(67, 87, 77));
+		region.subtract(circle(20, 87, 47));
+		region.subtract(circle(20, 87, 113));
+		
+		
+		shell = swt.shell("Regions on a Control", size:[200,200], background: "#ff0000") {
+			fillLayout()
+			def b2 = button("Button with Regions", location:[100,100], region: region) {
+				onEvent(type:'Selection', closure: {shell.close()})
+			}
+		}
+		
+		shell.open()
+		while(! shell.isDisposed()) { 
+			if (! shell.display.readAndDispatch()) {
+				shell.display.sleep();
+			}
+		}
+		
+	}
+
+}
---
+++ src/examples/groovy/swt/examples/snippets/Snippet299.groovy
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package groovy.swt.examples.snippets
+
+import groovy.swt.SwtBuilder
+import org.eclipse.swt.layout.GridData
+
+
+/**
+ * For testing a 3.4 new feature
+ * 
+ * @author Frank
+ *
+ */
+public class Snippet299{
+
+	def swt = new SwtBuilder()
+	
+    public static void main(String[] args) {
+		def demo = new Snippet299()
+		demo.run()
+	}
+	
+	void run(){
+		
+		def shell = swt.shell("snippet299", size:[250,100]) {
+			rowLayout(center:true)
+			button("Button 0")
+			button("Button 1") {
+				rowData (height: 50)
+			}
+			button("Button 2") {
+				rowData (height: 70)
+			}
+			button("Button 3")
+		}
+		
+		shell.open()
+		while(! shell.isDisposed()) { 
+			if (! shell.display.readAndDispatch()) {
+				shell.display.sleep();
+			}
+		}
+		
+	}
+
+}
