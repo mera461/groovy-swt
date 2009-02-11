d2c7b5227f48654f602d0eabb0cae872a813def0
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-02-11 12:09:14 +0000

    Threading examples.

---
+++ trunk/groovy/modules/groovy-swt/src/examples/groovy/swt/examples/ThreadingDemo.groovy
@@ -0,0 +1,43 @@
+package groovy.swt.examples
+
+import groovy.swt.SwtBuilder
+import org.eclipse.swt.layout.GridData
+    
+
+public class ThreadingDemo{
+	
+    def swt = new SwtBuilder()
+    def counterLabel
+    def counter = 0
+    
+	public static void main(String[] args) {
+    	def demo = new ThreadingDemo()
+    	demo.run()
+    }
+    
+    def run(){
+        
+        def shell = swt.shell ('ThreadingDemo', size:[100,100] ) {
+        	rowLayout()
+        	counterLabel = label('Starting ....')
+        }
+        
+        Thread.start {
+        	Thread.sleep(3000)
+        	while (true) {
+        		Thread.sleep(1000)
+        		counter++
+        		SwtBuilder.asyncExec {
+        			counterLabel.text = counter
+        		}
+        	}
+        }
+    	
+		shell.open()
+		while(! shell.isDisposed()) { 
+			if (! shell.display.readAndDispatch()) {
+				shell.display.sleep();
+			}
+		}
+    }
+}
---
+++ trunk/groovy/modules/groovy-swt/src/examples/groovy/swt/examples/ThreadingProgressBarDemo.groovy
@@ -0,0 +1,45 @@
+package groovy.swt.examples
+
+import groovy.swt.SwtBuilder
+import org.eclipse.swt.layout.GridData
+    
+
+public class ThreadingProgressBarDemo{
+	
+    def swt = new SwtBuilder()
+    def counterLabel
+    def counter = 0
+    
+	public static void main(String[] args) {
+    	def demo = new ThreadingProgressBarDemo()
+    	demo.run()
+    }
+    
+    def run(){
+
+    	def bar = null
+        def shell = swt.shell ('ThreadingDemo', size:[150,100] ) {
+        	migLayout(columnConstraints:"[right][grow]")
+        	label("Updating:")
+        	bar = progressBar(style:"smooth", bounds:[10, 10, 200, 32])
+        }
+    	
+    	final int maximum = bar.maximum
+        
+        Thread.start {
+			for (final int[] i = new int[1]; i[0] <= maximum; i[0]++) {
+				Thread.sleep(100)
+				swt.asyncExec {
+					if (! bar.isDisposed ()) bar.setSelection(i[0])
+				}
+			}
+        }
+    	
+		shell.open()
+		while(! shell.isDisposed()) { 
+			if (! shell.display.readAndDispatch()) {
+				shell.display.sleep();
+			}
+		}
+    }
+}
