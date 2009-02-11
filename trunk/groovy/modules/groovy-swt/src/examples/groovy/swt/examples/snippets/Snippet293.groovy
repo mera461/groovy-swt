f7edf6d082fa8b86892068400e1b6afc97ac2336
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-02-11 12:11:09 +0000

    Eclipse 3.4 snippets.

---
+++ trunk/groovy/modules/groovy-swt/src/examples/groovy/swt/examples/snippets/Snippet293.groovy
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package groovy.swt.examples.snippets
+
+import groovy.swt.SwtBuilder
+import org.eclipse.swt.layout.GridData
+
+
+/**
+ * For testing a 3.4 new feature: Tristate buttons
+ * 
+ * @author Frank
+ *
+ */
+public class Snippet293{
+
+	def swt = new SwtBuilder()
+	
+    public static void main(String[] args) {
+		def demo = new Snippet293()
+		demo.run()
+	}
+	
+	void run(){
+		
+		def shell = swt.shell("snippet293") {
+			gridLayout()
+			button("State 1", style:'check', selection: true)
+			button("State 2", style:'check', selection: false)
+			button("State 3", style:'check', selection: true, grayed: true)
+			button("State 4", style:'check', selection: true, enabled: false)
+		}
+		
+		shell.open()
+		while(! shell.isDisposed()) { 
+			if (! shell.display.readAndDispatch()) {
+				shell.display.sleep();
+			}
+		}
+		
+	}
+
+}
