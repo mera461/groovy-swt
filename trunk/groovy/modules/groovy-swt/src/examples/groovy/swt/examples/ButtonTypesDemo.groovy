e6b52fca257e58862e3c8c2af08411784b3f75d4
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-02-11 12:09:49 +0000

    Different type of buttons.

---
+++ trunk/groovy/modules/groovy-swt/src/examples/groovy/swt/examples/ButtonTypesDemo.groovy
@@ -0,0 +1,34 @@
+package groovy.swt.examples
+
+import groovy.swt.SwtBuilder
+import org.eclipse.swt.layout.GridData
+    
+
+public class ButtonTypesDemo{
+	
+    def swt = new SwtBuilder()
+    
+	public static void main(String[] args) {
+    	def demo = new ButtonTypesDemo()
+    	demo.run()
+    }
+    
+    def run(){
+        
+        def shell = swt.shell ('The Button types Demo', size:[700,700] ) {
+        	rowLayout()
+        	button("Check 1", style:"check")
+        	button("Push 1")
+        	button("Radio 1", style:"radio")
+        	button("Push 2")
+		}
+    	
+    	shell.open()
+	
+		while(! shell.isDisposed()) { 
+			if (! shell.display.readAndDispatch()) {
+				shell.display.sleep();
+			}
+		}
+    }
+}
