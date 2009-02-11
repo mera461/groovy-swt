82eea64e1eed1371d59960380ddcbd270a7ad53e
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-02-11 12:42:08 +0000

    Testing mixing swt and swing.

--- trunk/groovy/modules/groovy-swt/src/examples/groovy/swt/examples/AwtSwtDemo.groovy
+++ trunk/groovy/modules/groovy-swt/src/examples/groovy/swt/examples/AwtSwtDemo.groovy
@@ -1,28 +1,40 @@
 package groovy.swt.examples
 
 import groovy.swt.SwtBuilder
-import groovy.swt.CustomSwingBuilder
+import groovy.swing.SwingBuilder
+import java.awt.BorderLayout
+
+/*
+ * See SWT snippet 154 for flicker free integration.
+ * http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet154.java?view=co
+ *
+ * 
+ */
+
 
 class AwtSwtDemo {
     
     def swt
     def swing
     def shell
+    def count = 0
+    def textlabel
     
     def run() {
         swt = new SwtBuilder()
-   		swing = new CustomSwingBuilder()        
+   		swing = new SwingBuilder()        
    		
-        shell = swt.shell ( text:'The AwtSwt Demo' ) {
+        shell = swt.shell ( 'The AwtSwt Demo', size:[200,100] ) {
          	fillLayout()
          	
-         	label( text:"this is a swt label" )
+         	label("this is a swt label" )
          	
          	composite ( style:"border, embedded" ) {
          		fillLayout()
-         	
-	         	//swing.awtFrame()
-				swing.tree()
+         		//label('Another swt label')
+	         	awtFrame() {
+        			swing.label('Swing label')
+         		}
 			}
         }
         
