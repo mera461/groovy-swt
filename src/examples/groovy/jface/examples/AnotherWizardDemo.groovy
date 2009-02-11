b3818a04b442dd6409c7197f0695d49af52379cd
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-02-11 12:07:57 +0000

    Removed default 'text:' attributes.

--- src/examples/groovy/jface/examples/AnotherWizardDemo.groovy
+++ src/examples/groovy/jface/examples/AnotherWizardDemo.groovy
@@ -45,7 +45,7 @@ class AnotherWizardDemo {
 					  	def Calendar cal = Calendar.getInstance()
 					  	cal.setTime(new Date())
 				        def day = cal.get(Calendar.DAY_OF_MONTH)
-						label (  text:"Travel on:", layoutData:gridData(horizontalAlignment:GridData.BEGINNING))
+						label ("Travel on:", layoutData:gridData(horizontalAlignment:GridData.BEGINNING))
 					    def travelDate = combo( style:"READ_ONLY", items:dates, visibleItemCount:10, layoutData:gridData(horizontalAlignment:GridData.FILL, widthHint:25))
 					    travelDate.text=day
 						def travelMonth = combo(  style:"READ_ONLY", items: months, layoutData:gridData(horizontalAlignment:GridData.FILL))
@@ -62,22 +62,22 @@ class AnotherWizardDemo {
 					   	travelYear.text = startyear
 						
 						// Date of return		
-						label (style:"NONE", text:"Return on:", layoutData:gridData(horizontalAlignment:GridData.FILL))
+						label (style:"NONE", "Return on:", layoutData:gridData(horizontalAlignment:GridData.FILL))
 
 						combo(style:"READ_ONLY", items: dates, layoutData:gridData(horizontalAlignment:GridData.FILL, widthHint:25))
 						combo( style:"READ_ONLY", items:months, layoutData:gridData(horizontalAlignment:GridData.FILL))
 						combo(style:"READ_ONLY", items:years, layoutData:gridData(horizontalAlignment:GridData.FILL))
 						
 						// Departure				
-						label (style:"NONE", text:"From:", layoutData:gridData(horizontalAlignment:GridData.FILL))			
+						label (style:"NONE", "From:", layoutData:gridData(horizontalAlignment:GridData.FILL))			
 						text( style:"BORDER", layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3));
 
 						// Destination
-						label (style:"NONE", text:"To:", layoutData:gridData(horizontalAlignment:GridData.FILL))				
+						label (style:"NONE", "To:", layoutData:gridData(horizontalAlignment:GridData.FILL))				
 						text( style:"BORDER", layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3))
 						
-						radioButton(  text:"Take a plane", layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3))
-						radioButton( text: "Take the car", layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3))
+						radioButton("Take a plane", layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3))
+						radioButton("Take the car", layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3))
 						line (layoutData:gridData(horizontalAlignment:GridData.FILL, horizontalSpan:3))
 					}
 	  			})
@@ -87,7 +87,7 @@ class AnotherWizardDemo {
 	  					closure: { 
 						jface.composite( it ) {
 							gridLayout( numColumns:"2" )
-							label( text:"Set PageComplete to true/false: " )
+							label( "Set PageComplete to true/false: " )
 							    def button2 = button(style:"PUSH", text:"Change it") {
         							onEvent(type:"Selection", closure:{ 
 							        	wizardPage2.setPageComplete(!wizardPage2.isPageComplete() )
@@ -98,8 +98,8 @@ class AnotherWizardDemo {
 				def wizardPage3 = wizardPage( title:"Step 3", description:"This is the third page", closure: { 
 						jface.composite( it ) {
 							gridLayout( numColumns:"2" )
-							label( text:"Label 3" )
-							button( text:"Do nothing" )
+							label("Label 3" )
+							button("Do nothing" )
 						}
 				})
 					
