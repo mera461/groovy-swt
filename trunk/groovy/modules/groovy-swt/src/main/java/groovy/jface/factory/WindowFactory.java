4ca3940f2a957490b0fb5917a5f484c24689a9c4
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-02-16 14:54:11 +0000

    An ApplicationWindow will automatically call update() on menuManager, coolbarManager and toolBarManager.

--- trunk/groovy/modules/groovy-swt/src/main/java/groovy/jface/factory/WindowFactory.java
+++ trunk/groovy/modules/groovy-swt/src/main/java/groovy/jface/factory/WindowFactory.java
@@ -4,6 +4,7 @@
  */
 package groovy.jface.factory;
 
+import groovy.jface.impl.ApplicationWindowImpl;
 import groovy.swt.convertor.PointConverter;
 import groovy.swt.factory.WidgetFactory;
 import groovy.util.FactoryBuilderSupport;
@@ -11,6 +12,9 @@ import groovy.util.FactoryBuilderSupport;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.jface.action.CoolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.ToolBarManager;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Shell;
@@ -67,4 +71,32 @@ public class WindowFactory extends WidgetFactory {
         setBeanProperties(window, attributes);
         return window;
     }
+	
+	@Override
+	public void onNodeCompleted(FactoryBuilderSupport builder, Object parent,
+			Object node) {
+		super.onNodeCompleted(builder, parent, node);
+		
+		// Because of a bug in eclipse 3.4
+		// https://bugs.eclipse.org/bugs/show_bug.cgi?id=243758
+		// you need to update all menuManagers
+		
+		if (node instanceof ApplicationWindowImpl) {
+			ApplicationWindowImpl window = (ApplicationWindowImpl) node;
+			
+			MenuManager menuManager = window.getMenuBarManager();
+			if (menuManager != null) {
+				menuManager.updateAll(true);
+			}
+			CoolBarManager coolBarManager = window.getCoolBarManager();
+			if (coolBarManager != null) {
+				coolBarManager.update(true);
+			}
+			ToolBarManager toolBarManager = window.getToolBarManager();
+			if (toolBarManager != null) {
+				toolBarManager.update(true);
+			}
+		}
+	}
+	
 }
