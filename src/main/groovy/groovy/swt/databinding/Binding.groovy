88633cefef7630dca05f403d57fe7bf0c820ca38
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-03-25 10:00:37 +0000

    First go at databinding.

---
+++ src/main/groovy/groovy/swt/databinding/Binding.groovy
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package groovy.swt.databinding
+
+
+
+/**
+ * @author Frank
+ *
+ */
+public class Binding{
+	Object model
+	/* modelProperty: a single name given the property of the model
+	 * or a list of names.
+	 */
+	Object modelProperty
+	// used for viewers when the modelProperty is a list of objects
+	// select which properties of the object to display
+	//def listProperties
+}
