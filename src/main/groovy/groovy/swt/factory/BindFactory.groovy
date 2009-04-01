131548b8dcf2fbd949a4ca38a2f5a97fd6a21156
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-04-01 06:04:06 +0000

    Added handling of UpdateStraties for model2target and target2model

--- src/main/groovy/groovy/swt/factory/BindFactory.groovy
+++ src/main/groovy/groovy/swt/factory/BindFactory.groovy
@@ -10,18 +10,29 @@ import org.codehaus.groovy.binding.*
 
 import org.eclipse.core.databinding.beans.BeansObservables
 import org.eclipse.core.databinding.beans.BeanProperties
+import org.eclipse.core.databinding.conversion.IConverter
 import org.eclipse.core.databinding.observable.IObservable
 import org.eclipse.core.databinding.observable.Observables
+import org.eclipse.core.databinding.observable.list.WritableList
+import org.eclipse.core.databinding.observable.list.IObservableList;
 import org.eclipse.core.databinding.observable.map.IObservableMap
-
+import org.eclipse.core.databinding.observable.set.IObservableSet;
+import org.eclipse.core.databinding.observable.Realm
+import org.eclipse.core.databinding.property.Properties
+import org.eclipse.core.databinding.UpdateValueStrategy
+import org.eclipse.core.databinding.validation.IValidator
 
 import org.eclipse.jface.databinding.swt.ISWTObservableValue
 import org.eclipse.jface.databinding.swt.SWTObservables
 import org.eclipse.jface.databinding.viewers.ObservableListContentProvider
 import org.eclipse.jface.databinding.viewers.ObservableMapLabelProvider
 import org.eclipse.jface.databinding.viewers.ViewerSupport
+import org.eclipse.jface.databinding.viewers.ViewersObservables
 import org.eclipse.jface.viewers.StructuredViewer
+import org.eclipse.jface.viewers.TableViewer
 import org.eclipse.swt.SWT
+import org.eclipse.swt.widgets.TableColumn
+
 
 /**
  */
@@ -56,7 +67,10 @@ public class BindFactory extends AbstractFactory {
 
         Object model = attributes.remove("model") ?: value
         Object modelProperty = attributes.remove("modelProperty")
+        
         Binding binding = new Binding(model:model, modelProperty:modelProperty)
+        
+        extractUpdateValueStrategy(attributes, binding)
         
         return binding
     }
@@ -87,7 +101,7 @@ public class BindFactory extends AbstractFactory {
             }
             
             // viewer or simple widget?
-            if (node instanceof StructuredViewer && property=='input') {
+            if (node instanceof StructuredViewer && property.toLowerCase()=='input') {
             	bindViewerInput(builder, node, property, (Binding) value)
             } else {
             	bindSimpleWidget(builder, node, property, (Binding) value)
@@ -100,30 +114,81 @@ public class BindFactory extends AbstractFactory {
 
     def bindViewerInput(FactoryBuilderSupport builder, StructuredViewer node, def property, Binding binding) {
     	
-    	// NOTE: If I do a BeansObservables.observeList(binding.model, binding.modelProperty)
-    	// then it will not monitor changes in the list, but only new assignments to the variable
+        Object columnProperties = binding.modelProperty
+        if (columnProperties instanceof GString) columnProperties = columnProperties.toString()
+        
+        // if the model is a writable list of objects, then the columnProperty is a property
+        // of that object.
+        if (binding.model instanceof WritableList) {
+            if (columnProperties instanceof String) columnProperties = [columnProperties]
+        	// NOTE: If I do a BeansObservables.observeList(binding.model, binding.modelProperty)
+        	// then it will not monitor changes in the list, but only new assignments to the variable
+        	ViewerSupport.bind(
+            	    node,
+            	    binding.model,
+            	    BeanProperties.values(columnProperties as String[]) );
+        } else if (binding.model instanceof IObservableList
+        	|| binding.model instanceof IObservableSet) {
+            if (columnProperties instanceof String) columnProperties = [columnProperties]
+        	ViewerSupport.bind(
+            	    node,
+            	    binding.model,
+            	    BeanProperties.values(columnProperties as String[]) );
+        } else if (binding.model instanceof ArrayList) {
+        	println "WARNING: Wrapping an ArrayList in a WritableList. Updating is not possible"
+        	def l = new WritableList(Realm.default)
+        	l.addAll(binding.model)
+        	binding.model = l
+        	println l[1]
+        	ViewerSupport.bind(
+            	    node,
+            	    binding.model,
+            	    BeanProperties.values(columnProperties as String[]) );
+        } else {
+			ViewerSupport.bind(node,
+							   BeansObservables.observeList(binding.model, columnProperties, String.class),
+							   Properties.selfValue(String.class));
+        }
     	
-        Object listProperties = binding.modelProperty
-        if (listProperties instanceof GString) listProperties = listProperties.toString()
-        if (listProperties instanceof String) listProperties = [listProperties]
-    	ViewerSupport.bind(
-    	    node,
-			binding.model,
-    	    BeanProperties.values(listProperties as String[] ) );
+    	
+    	// It it is a TableViewer and no tablecolumns defined then provide some
+    	// reasonably defaults with the given properties
+    	if (node instanceof TableViewer
+    		&& node.table.columnCount == 0) {
+    		// create some table columns
+    		// TODO: TableViewerColumn
+    		columnProperties.each {
+    			def col = new TableColumn(node.table, SWT.LEFT)
+    			col.text = it
+    			col.width = 100
+    			col.pack()
+    		}
+    		node.table.headerVisible = true
+    		node.table.linesVisible = true 
+    	}
     }    
     
-    def bindSimpleWidget(FactoryBuilderSupport builder, def node, def property, PropertyBinding pb) {
+    def bindSimpleWidget(FactoryBuilderSupport builder, def node, def property, Binding binding) {
         // target observable
         def targetObservable = createTargetObservable(builder, node, property)
         
         // model observable
         Object properties = binding.modelProperty
-        if (properties instanceof GString) properties = listProperties.toString()
-        def modelObservable = createBeanObservable(builder, pb.bean, properties)
+        if (properties instanceof GString) properties = properties.toString()
+        
+        // wrap an array list in a WritableList
+        if (binding.model instanceof ArrayList) {
+        	println "WARNING: Wrapping an ArrayList in a WritableList. Updating is not possible"
+        	def l = new WritableList(Realm.default)
+        	l.addAll(binding.model)
+        	model.bidnding = l
+        }
+
+        def modelObservable = createBeanObservable(builder, binding.model, properties)
 
         // create the binding
-        def dbc = findDataBindingContext(builder, node)
-        dbc.bindValue(targetObservable, modelObservable, null, null)
+        def dbc = builder.dataBindingContext
+        dbc.bindValue(targetObservable, modelObservable, binding.target2model, binding.model2target)
     }
 
     ISWTObservableValue createTargetObservable(FactoryBuilderSupport builder, def node, def attribute) {
@@ -178,22 +243,70 @@ public class BindFactory extends AbstractFactory {
     }
     
     IObservable createBeanObservable(FactoryBuilderSupport builder, def bean, def propertyName) {
-    	
-    	return BeansObservables.observeValue(bean, propertyName)
+    	IObservable observable
+    	if (bean instanceof IObservable) {
+    		observable = bean
+    	} else if (bean instanceof StructuredViewer) {
+    		def selectedItem = ViewersObservables.observeSingleSelection(bean)
+    		observable = BeansObservables.observeDetailValue(selectedItem, propertyName)
+    	} else {
+    		observable = BeansObservables.observeValue(bean, propertyName) 
+    	}
+    	return observable
     }
 
     /*
-     * Look upward in the node hiearachy and find a DataBindingContext
-     * to use for the bindings.
+     * Set all UpdateValueStrategy attributes on the binding.
      */
-    def findDataBindingContext(def builder, def node) {
-    	def context = builder.context
-        def dbc = null
-        while (dbc == null && context!=null) {
-            dbc = context[SwtBuilder.DATA_BINDING_CONTEXT]
-        	context = context[FactoryBuilderSupport.PARENT_CONTEXT]
-        }
-    	return dbc
+    def extractUpdateValueStrategy(Map attributes, Binding binding) {
+    	binding.model2target =  extractUpdateValueStrategy(attributes, binding, 'model2target')
+    	binding.target2model =  extractUpdateValueStrategy(attributes, binding, 'target2model')
+    }
+
+    UpdateValueStrategy extractUpdateValueStrategy(Map attributes, Binding binding, String prefix) {
+    	UpdateValueStrategy strategy = null
+    	// handle the model2target:[] part
+    	def strategyMap = attributes.remove(prefix)
+    	if (strategyMap!=null) {
+    		strategy = updateStategyAttributes(strategy, strategyMap, '')
+    	}
+
+    	// handle the model2target.converter:{} part
+		strategy = updateStategyAttributes(strategy, attributes, prefix+'.')
+    	return strategy
+    }
+    
+    UpdateValueStrategy updateStategyAttributes(UpdateValueStrategy strategy, Map attributes, String prefix) {
+    	def policy = attributes.remove(prefix+'policy')
+    	if (strategy==null && policy!=null) strategy=new UpdateValueStrategy(true, convertPolicy(policy))
+    	['afterConvertValidator', 'afterGetValidator', 'beforeSetValidator', 'converter'].each {
+        	def value = attributes.remove(prefix+it)
+        	if (value != null) {
+        		if (strategy==null) strategy=new UpdateValueStrategy()
+        		if (it=='converter') {
+        			strategy.converter = value as IConverter
+        		} else {
+        			strategy."$it" = value as IValidator
+        		}
+        		
+        	}
+    	}
+    	return strategy
+    }
+    
+    int convertPolicy(String policy) {
+    	switch(policy.toLowerCase()) {
+    	case 'convert':
+    		return UpdateValueStrategy.POLICY_CONVERT
+    	case 'never':
+    		return UpdateValueStrategy.POLICY_NEVER
+    	case 'onRequest':
+    		return UpdateValueStrategy.POLICY_ON_REQUEST
+    	case 'update':
+    		return UpdateValueStrategy.POLICY_UPDATE
+    	default:
+    		throw new RuntimeException("Invalid UpdateStrategy policy: $policy. It should be [never, convert, onRequest, update].")
+    	}
     }
     
 }
