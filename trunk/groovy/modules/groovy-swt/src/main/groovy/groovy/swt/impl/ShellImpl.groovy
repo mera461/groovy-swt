2b0ff443a86d863e290f0a0b39b0a11187a3595d
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-05-01 13:10:13 +0000

    Converted ShellImpl to groovy.

---
+++ trunk/groovy/modules/groovy-swt/src/main/groovy/groovy/swt/impl/ShellImpl.groovy
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package groovy.swt.impl
+
+import org.eclipse.swt.widgets.Display
+import org.eclipse.swt.widgets.Shell
+
+/**
+ * @author Frank
+ * 
+ */
+public class ShellImpl extends Shell {
+
+	public ShellImpl() {super()}
+	public ShellImpl(Display display) {super(display)}
+	public ShellImpl(Display display, int style)  {super(display, style)}
+	public ShellImpl(int style)  {super(style)}
+	public ShellImpl(Shell parent)  {super(parent)}
+	public ShellImpl(Shell parent, int style) 	  {super(parent, style)}
+
+	public void doMainloop(boolean doPack=true) {
+		if (doPack) this.pack()
+		open()
+		while (!isDisposed()) {
+			if (!getDisplay().readAndDispatch()) {
+				getDisplay().sleep()
+			}
+		}
+	}
+	
+	protected void checkSubclass() {}
+}
