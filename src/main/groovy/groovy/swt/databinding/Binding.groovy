14d156a8270d4d3ae43be498080373155728b8c4
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-05-01 13:09:29 +0000

    Added type, target, and children (for trees)

--- src/main/groovy/groovy/swt/databinding/Binding.groovy
+++ src/main/groovy/groovy/swt/databinding/Binding.groovy
@@ -11,11 +11,26 @@ import org.eclipse.core.databinding.UpdateValueStrategy
  *
  */
 public class Binding{
+	// Value, Set, List
+	// Set is currently only used for checkElements of a checkTableViewer
+	String bindType = 'Value'
+	
+	Object target
+	
+	Object targetProperty
+	
 	Object model
+	
 	/* modelProperty: a single name given the property of the model
 	 * or a list of names.
 	 */
 	Object modelProperty
+	
+	
+	/* modelProperty: the property to use as the children of an element
+	 */
+	Object childrenProperty
+	
 	// used for viewers when the modelProperty is a list of objects
 	// select which properties of the object to display
 	//def listProperties
