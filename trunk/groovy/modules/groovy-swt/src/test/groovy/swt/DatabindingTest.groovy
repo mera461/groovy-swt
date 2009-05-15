824ccf4c9dfb09df94887ad08659b129484dc103
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-05-15 08:42:15 +0000

    Updated

--- trunk/groovy/modules/groovy-swt/src/test/groovy/swt/DatabindingTest.groovy
+++ trunk/groovy/modules/groovy-swt/src/test/groovy/swt/DatabindingTest.groovy
@@ -37,30 +37,79 @@ public class DatabindingTest extends TestCase {
 	WritableList list 
 	
 	def jface = new JFaceBuilder()
-	 
-    public void testSimpel() {
+
+	/*
+	 * Test basic binding
+	 */
+    public void testSimpleAsAttributeDelegate() {
 		//def t1, t2
         def shell = jface.shell() {
         	text(id:'t1', text: bind(model: this, modelProperty:'text'))
         	text(id:'t2', text: bind(model: this, modelProperty:'number'))
         }
         
+		// change the model
         text = "something"
         jface.dataBindingContext.updateTargets()
         assert jface.t1.text == "something"
 
+        // change the widget
         jface.t1.text = "something else"
         jface.dataBindingContext.updateModels()
         assert text == "something else"
+        
+        // test the number
+        jface.t2.text = 8
+        jface.dataBindingContext.updateModels()
+        assert number == 8
+        
+        // NOTE: When it is on the same object, you need to do a set..() to trigger
+        // the change events, otherwise it acts as a direct access.
+        setNumber(7)
+        jface.dataBindingContext.updateModels()
+        assert jface.t2.text == '7'
+        
+        // dispose the shell
         shell.dispose()
     }
     
-    public void testSimple() {
-          jface.shell('title') {
-          }
-      }
-    
-    
+    public void testSimpleAsVerbose() {
+		//def t1, t2
+        def shell = jface.shell() {
+        	text(id:'t1')
+        	text(id:'t2')
+        	bind(target:t1, targetProperty:'text', model: this, modelProperty:'text')
+        	bind(target:t2, targetProperty:'text', model: this, modelProperty:'number')
+        }
+        
+		// change the model
+        text = "x1"
+        jface.dataBindingContext.updateTargets()
+        assert jface.t1.text == "x1"
+
+        // change the widget
+        jface.t1.text = "x2"
+        jface.dataBindingContext.updateModels()
+        assert text == "x2"
+        
+        // test the number
+        jface.t2.text = 9
+        jface.dataBindingContext.updateModels()
+        assert number == 9
+        
+        
+        // NOTE: When it is on the same object, you need to do a set..() to trigger
+        // the change events, otherwise it acts as a direct access.
+        setNumber(10)
+        jface.dataBindingContext.updateModels()
+        assert jface.t2.text == '10'
+        
+        // dispose the shell
+        shell.dispose()
+    }
+    /*
+     * Test binding to a empty list. There was a bug using ?: on a empty list :-)
+     */
     public void testBindingToEmptyList() {
     	list = new WritableList(jface.realm)
         def shell = jface.shell() {
