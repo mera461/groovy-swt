2add6e639ad65042d003d45684749ee8ba74f85a
frank.tolstrup <frank.tolstrup@a5544e8c-8a19-0410-ba12-f9af4593a198>
2009-05-27 08:48:21 +0000

    Added tests for submenus.

--- src/test/groovy/swt/SwtTest.groovy
+++ src/test/groovy/swt/SwtTest.groovy
@@ -17,8 +17,6 @@ import org.eclipse.swt.widgets.Event
 import org.eclipse.swt.widgets.Shell
 
 /**
- * @author <a href="mailto:ckl@dacelo.nl">Christiaan ten Klooster </a>
- * @version $Revision: 915 $
  */
 public class SwtTest extends TestCase {
     public void testSwt() {
@@ -87,5 +85,57 @@ public class SwtTest extends TestCase {
     void pushButton(def button) {
     	button.notifyListeners(SWT.Selection, null)
     }
+    
+    public void testMenuItem() {
+        def swt = new SwtBuilder()
+        swt.shell('title') {
+        	rowLayout()
+        	menu() {
+        		menuItem('text', accelerator: 'Ctrl+S', id:'m1')
+        		menuItem('text', accelerator: SWT.CTRL | (int)'S', id:'m2')
+        	}
+        }
+        
+        assertEquals swt.m1.accelerator, swt.m2.accelerator 
+    }
+    
+    public void testTableMenu() {
+        def swt = new SwtBuilder()
+        swt.shell('title') {
+        	rowLayout()
+        	table(id:'t1') {
+        		menu() {
+        			menuItem('text', accelerator: 'Ctrl+S', id:'m1')
+        		}
+        	}
+        }
+    	assertNotNull swt.t1.menu
+    }
+
+    public void testMenu() {
+        def swt = new SwtBuilder()
+        def x = 1
+        def localVar = null
+        swt.shell('title') {
+			submenu = swt.menu( style:"BAR") {
+				menuItem('File', id:'file', style:'CASCADE') {
+					menu(id:'m2') {
+						menuItem('Save')
+						menuItem('Save as', id:'saveas', style:'CASCADE') {
+							menu(id:'m3') {
+								menuItem('word')
+								menuItem('excel')
+								menuItem('powerpoint')
+							}
+						}
+						menuItem('E&xit')
+					}
+				}
+			}
+        }
+        assert swt.file.menu == swt.m2
+        assert swt.saveas.menu == swt.m3
+    }
+
     
 }
